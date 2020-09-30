a06f48be1c8cc258d3931b2e926621423647dd20
Edgar Tulodziecki <15g16480@marling.gloucs.sch.uk>
2020-09-30 13:25:42 +0000

    classes with extension

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ class Fruit{
     this.fruit = fruit;
   }
   sayYummy() {
-    console.log(this.fruit)
+    return 0
   }
 }
+class Melon extends Fruit {
+  constructor(circumference) {
+    super('Melon')
+    this.circumference = circumference;
+  }
+doMelon() {
+  return (this.circumference + 20)
+}
+}
 
-let apple = new Fruit('apple pie')
-let orange = new Fruit('orange juice')
-
-let fruits = [apple, orange]
-fruits.forEach(x => x.sayYummy())
+let fruits = [
+  new Melon(2),
+  new Melon(4)
+]
+fruits.forEach(x => console.log(x.doMelon()))
